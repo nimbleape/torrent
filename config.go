9bb9349bd5d3367eacbb359232e8986c059b1bb0
marcovidonis <marco@nimblea.pe>
2023-10-27 14:08:40 +0100

    set up provision for onPeerConnUpdate function

--- config.go
+++ config.go
@@ -190,6 +190,9 @@ type ClientConfig struct {
 	DialRateLimiter *rate.Limiter
 
 	PieceHashersPerTorrent int // default: 2
+
+	// TODO something like this
+	OnPeerConnUpdate func()
 }
 
 func (cfg *ClientConfig) SetListenAddr(addr string) *ClientConfig {
