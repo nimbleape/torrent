81b55a0aca4cc4d9a92f0a30c8d3025b1b354393
marcovidonis <marco@nimblea.pe>
2023-11-24 14:05:56 +0000

    add unit tests on PeerConn Observer status reading

--- client-peerconn_test.go
+++ client-peerconn_test.go
@@ -1,11 +1,60 @@
 package torrent
 
 import (
+	"errors"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
 )
+
+func TestPeerConnObserverReadStatusOk(t *testing.T) {
+	cfg := TestingConfig(t)
+	cfg.DisableTrackers = false
+	cfg.EstablishedConnsPerTorrent = 1
+	cfg.Observers = &Observers{
+		Peers: PeerObserver{
+			PeerStatus: make(chan PeerStatus),
+		},
+	}
+
+	c, _ := NewClient(cfg)
+	defer c.Close()
+
+	go func() {
+		cfg.Observers.Peers.PeerStatus <- PeerStatus{
+			Ok: true,
+		}
+	}()
+
+	status := readChannelTimeout(t, cfg.Observers.Peers.PeerStatus, 500*time.Millisecond).(PeerStatus)
+	require.True(t, status.Ok)
+	require.Nil(t, status.Err)
+}
+
+func TestPeerConnObserverReadStatusErr(t *testing.T) {
+	cfg := TestingConfig(t)
+	cfg.DisableTrackers = false
+	cfg.EstablishedConnsPerTorrent = 1
+	cfg.Observers = &Observers{
+		Peers: PeerObserver{
+			PeerStatus: make(chan PeerStatus),
+		},
+	}
+
+	c, _ := NewClient(cfg)
+	defer c.Close()
+
+	go func() {
+		cfg.Observers.Peers.PeerStatus <- PeerStatus{
+			Err: errors.New("test error"),
+		}
+	}()
+
+	status := readChannelTimeout(t, cfg.Observers.Peers.PeerStatus, 500*time.Millisecond).(PeerStatus)
+	require.False(t, status.Ok)
+	require.EqualError(t, status.Err, "test error")
+}
 
 // TODO ideally we'd run both peers locally for these tests,
 // and transfer a very small file
