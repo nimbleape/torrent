d4799f90591411debc0401fedea452b990beafa1
marcovidonis <marco@nimblea.pe>
2023-11-30 11:56:06 +0000

    refactor test by doing direct transfer locally

--- client-peerconn_test.go
+++ client-peerconn_test.go
@@ -2,10 +2,18 @@ package torrent
 
 import (
 	"errors"
+	"io"
+	"os"
 	"testing"
+	"testing/iotest"
 	"time"
 
+	"github.com/anacrolix/missinggo/v2/bitmap"
+	"github.com/anacrolix/torrent/internal/testutil"
+	"github.com/frankban/quicktest"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/time/rate"
 )
 
 func TestPeerConnObserverReadStatusOk(t *testing.T) {
@@ -56,30 +64,154 @@ func TestPeerConnObserverReadStatusErr(t *testing.T) {
 	require.EqualError(t, status.Err, "test error")
 }
 
-// TODO ideally we'd run both peers locally for these tests,
-// and transfer a very small file
-
 func TestPeerConnEstablished(t *testing.T) {
-	cfg := TestingConfig(t)
-	cfg.DisableTrackers = false
-	cfg.EstablishedConnsPerTorrent = 1
-	cfg.Observers = NewClientObservers()
+	obs := NewClientObservers()
+	ps := testClientTransferParams{
+		ConfigureLeecher: ConfigureClient{
+			Config: func(cfg *ClientConfig) {
+				// TODO one of UTP or TCP is needed for the transfer
+				// Does this mean we're not doing webtorrent? TBC
+				// cfg.DisableUTP = true
+				cfg.DisableTCP = true
+				cfg.Debug = false
+				cfg.DisableTrackers = true
+				cfg.EstablishedConnsPerTorrent = 1
+				cfg.Observers = obs
+			},
+		},
+	}
 
-	c, _ := NewClient(cfg)
-	defer c.Close()
+	go testClientTransfer(t, ps)
 
-	// Sintel, a free, Creative Commons movie
-	const m = "magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent"
-
-	to, err := c.AddMagnet(m)
-	require.NoError(t, err)
-
-	<-to.GotInfo()
-	to.DownloadAll()
-
-	// need to give it enough time to connect to actual peers
-	status := readChannelTimeout(t, cfg.Observers.Peers.PeerStatus, 60*time.Second).(PeerStatus)
+	status := readChannelTimeout(t, obs.Peers.PeerStatus, 500*time.Millisecond).(PeerStatus)
 	// TODO a check about PeerID?
 	require.True(t, status.Ok)
 	require.Nil(t, status.Err)
+}
+
+type ConfigureClient struct {
+	Config func(cfg *ClientConfig)
+	Client func(cl *Client)
+}
+
+type testClientTransferParams struct {
+	SeederUploadRateLimiter    *rate.Limiter
+	LeecherDownloadRateLimiter *rate.Limiter
+	ConfigureSeeder            ConfigureClient
+	ConfigureLeecher           ConfigureClient
+
+	LeecherStartsWithoutMetadata bool
+}
+
+// Simplified version of testClientTransfer found in test/leecher-storage.go.
+// Could not import and reuse that function due to circular dependencies between modules.
+func testClientTransfer(t *testing.T, ps testClientTransferParams) {
+	greetingTempDir, mi := testutil.GreetingTestTorrent()
+	defer os.RemoveAll(greetingTempDir)
+	// Create seeder and a Torrent.
+	cfg := TestingConfig(t)
+	cfg.Seed = true
+	// Some test instances don't like this being on, even when there's no cache involved.
+	cfg.DropMutuallyCompletePeers = false
+	if ps.SeederUploadRateLimiter != nil {
+		cfg.UploadRateLimiter = ps.SeederUploadRateLimiter
+	}
+	cfg.DataDir = greetingTempDir
+	if ps.ConfigureSeeder.Config != nil {
+		ps.ConfigureSeeder.Config(cfg)
+	}
+	seeder, err := NewClient(cfg)
+	require.NoError(t, err)
+	if ps.ConfigureSeeder.Client != nil {
+		ps.ConfigureSeeder.Client(seeder)
+	}
+	seederTorrent, _, _ := seeder.AddTorrentSpec(TorrentSpecFromMetaInfo(mi))
+	defer seeder.Close()
+	<-seederTorrent.Complete.On()
+
+	// Create leecher and a Torrent.
+	leecherDataDir := t.TempDir()
+	cfg = TestingConfig(t)
+	// See the seeder client config comment.
+	cfg.DropMutuallyCompletePeers = false
+	cfg.DataDir = leecherDataDir
+	if ps.LeecherDownloadRateLimiter != nil {
+		cfg.DownloadRateLimiter = ps.LeecherDownloadRateLimiter
+	}
+	cfg.Seed = false
+	if ps.ConfigureLeecher.Config != nil {
+		ps.ConfigureLeecher.Config(cfg)
+	}
+	leecher, err := NewClient(cfg)
+	require.NoError(t, err)
+	defer leecher.Close()
+	if ps.ConfigureLeecher.Client != nil {
+		ps.ConfigureLeecher.Client(leecher)
+	}
+	leecherTorrent, new, err := leecher.AddTorrentSpec(func() (ret *TorrentSpec) {
+		ret = TorrentSpecFromMetaInfo(mi)
+		ret.ChunkSize = 2
+		if ps.LeecherStartsWithoutMetadata {
+			ret.InfoBytes = nil
+		}
+		return
+	}())
+	require.NoError(t, err)
+	assert.False(t, leecherTorrent.Complete.Bool())
+	assert.True(t, new)
+
+	added := leecherTorrent.AddClientPeer(seeder)
+	assert.False(t, leecherTorrent.Seeding())
+	// The leecher will use peers immediately if it doesn't have the metadata. Otherwise, they
+	// should be sitting idle until we demand data.
+	if !ps.LeecherStartsWithoutMetadata {
+		assert.EqualValues(t, added, leecherTorrent.Stats().PendingPeers)
+	}
+	if ps.LeecherStartsWithoutMetadata {
+		<-leecherTorrent.GotInfo()
+	}
+	r := leecherTorrent.NewReader()
+	defer r.Close()
+	go leecherTorrent.SetInfoBytes(mi.InfoBytes)
+
+	assertReadAllGreeting(t, r)
+	<-leecherTorrent.Complete.On()
+	assert.NotEmpty(t, seederTorrent.PeerConns())
+	leecherPeerConns := leecherTorrent.PeerConns()
+	if cfg.DropMutuallyCompletePeers {
+		// I don't think we can assume it will be empty already, due to timing.
+		// assert.Empty(t, leecherPeerConns)
+	} else {
+		assert.NotEmpty(t, leecherPeerConns)
+	}
+	foundSeeder := false
+	for _, pc := range leecherPeerConns {
+		completed := pc.PeerPieces().GetCardinality()
+		t.Logf("peer conn %v has %v completed pieces", pc, completed)
+		if completed == bitmap.BitRange(leecherTorrent.Info().NumPieces()) {
+			foundSeeder = true
+		}
+	}
+	if !foundSeeder {
+		t.Errorf("didn't find seeder amongst leecher peer conns")
+	}
+
+	seederStats := seederTorrent.Stats()
+	assert.True(t, 13 <= seederStats.BytesWrittenData.Int64())
+	assert.True(t, 8 <= seederStats.ChunksWritten.Int64())
+
+	leecherStats := leecherTorrent.Stats()
+	assert.True(t, 13 <= leecherStats.BytesReadData.Int64())
+	assert.True(t, 8 <= leecherStats.ChunksRead.Int64())
+
+	// Try reading through again for the cases where the torrent data size
+	// exceeds the size of the cache.
+	assertReadAllGreeting(t, r)
+}
+
+func assertReadAllGreeting(t *testing.T, r io.ReadSeeker) {
+	pos, err := r.Seek(0, io.SeekStart)
+	assert.NoError(t, err)
+	assert.EqualValues(t, 0, pos)
+	quicktest.Check(t, iotest.TestReader(r, []byte(testutil.GreetingFileContents)), quicktest.IsNil)
 }
